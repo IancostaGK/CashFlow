8e6f9fe59e8b60cb4ef81bec2a449d1c62433e7f
Ian Santos <iancosta99gk@gmail.com>
2025-01-13 09:56:22 -0300

    feat: Excel

---
+++ src/CashFlow.Application/UseCases/Expenses/Report/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -0,0 +1,82 @@
+﻿
+using AutoMapper;
+using CashFlow.Communication.Enums;
+using CashFlow.Domain.Reports;
+using CashFlow.Domain.Repositories.Expenses;
+using ClosedXML.Excel;
+
+namespace CashFlow.Application.UseCases.Expenses.Report.Excel;
+
+public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
+{
+
+    private readonly IExpensesReadOnlyRepository _repository;
+    private const string CURRENCY_SYMBOL = "R$";
+
+    public GenerateExpensesReportExcelUseCase(IExpensesReadOnlyRepository repository, IMapper mapper)
+    {
+        _repository = repository;
+    }
+
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+        var expenses = await _repository.FilterByMonth(month);
+        if (expenses.Count == 0) {
+            return [];
+        }
+
+        using var workbook = new XLWorkbook();
+
+        var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
+        InsertHeader(worksheet);
+
+        var row = 2;
+        foreach (var expense in expenses) {
+            worksheet.Cell($"A{row}").Value = expense.Title;
+            worksheet.Cell($"B{row}").Value = expense.Date;
+            worksheet.Cell($"C{row}").Value = ConvertPaymentType((PaymentType)expense.PaymentType);
+
+            worksheet.Cell($"D{row}").Value = expense.Amount;
+            worksheet.Cell($"D{row}").Style.NumberFormat.Format = $"-{CURRENCY_SYMBOL} #,##0.00";
+            
+            worksheet.Cell($"E{row}").Value = expense.Description;
+
+            row++;
+        }
+
+        worksheet.Columns().AdjustToContents();
+
+        var file = new MemoryStream();
+        workbook.SaveAs(file);
+
+        return file.ToArray();
+    }
+
+    private string ConvertPaymentType(PaymentType paymentType) {
+        return paymentType switch
+        {
+            PaymentType.Cash => "Dinheiro",
+            PaymentType.CreditCard => "Cartao de Credito",
+            PaymentType.EletronicTransfer => "Transferencia Eletronica",
+            PaymentType.DebitCard => "Cartao de Debito",
+            _ => string.Empty,
+        };
+    }
+
+    private void InsertHeader(IXLWorksheet worksheet)
+    {
+        worksheet.Cell("A1").Value = ResourceReportGenerationMessages.TITLE;
+        worksheet.Cell("B1").Value = ResourceReportGenerationMessages.DATE;
+        worksheet.Cell("C1").Value = ResourceReportGenerationMessages.PAYMENT_TYPE;
+        worksheet.Cell("D1").Value = ResourceReportGenerationMessages.AMOUNT;
+        worksheet.Cell("E1").Value = ResourceReportGenerationMessages.DESCRIPTION;
+        
+        worksheet.Cells("A1:E1").Style.Font.Bold = true;
+        worksheet.Cells("A1:E1").Style.Fill.BackgroundColor = XLColor.FromHtml("#F5C2B6");
+        worksheet.Cell("A1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("B1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("C1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cell("D1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
+        worksheet.Cell("E1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+    }
+}
